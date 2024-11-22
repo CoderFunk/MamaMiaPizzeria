7989ee6a8e9d6b4b4915b33937b56686e37b5d75
Néstor Andrés Muñoz Rojas <job.andresrojas@gmail.com>
2024-11-22 06:57:02 -0300

    Se sube el proyecto MammaMiaPizzeria
    Se sube el proyecto terminado del hito 1 de MammaMiaPizzeria

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+        <NavBar></NavBar>
+        <Home></Home>
+        <Footer></Footer>
+    </>
+  )
+}
+export default App;
